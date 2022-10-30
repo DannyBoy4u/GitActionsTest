cbdb1d9d804c2566c7826171e9da7fa3b35b2983
Daniel Howard <daniel.howard@selu.edu>
2022-10-30 18:48:58 -0500

    feat: Newest change

---
+++ ComboBoxContentControl.py
@@ -0,0 +1,11 @@
+class ComboBoxOption:
+    """
+    This class contains all properties and methods for a ComboBox list item.
+
+    :param str display_text: The display text of the option
+    :param str value: The value of the option
+    """
+
+    def __init__(self, display_text: str, value: str):
+        self.display_text = display_text
+        self.value = value
